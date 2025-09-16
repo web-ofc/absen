e063c8b7bfa7de80dd01454294c4093ac5b683c6
web-ofc <kandias203@gmail.com>
2025-09-16 16:44:18 +0700

    Deskripsi perubahan lo

--- routes/web.php
+++ routes/web.php
@@ -65,14 +65,9 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth'])->group(function () {
     // Roles dengan permission check
-    Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
-    Route::get('/roles/create', [RoleController::class, 'create'])->name('roles.create');
-    Route::post('/roles', [RoleController::class, 'store'])->name('roles.store');
-    Route::get('/roles/{role}', [RoleController::class, 'show'])->name('roles.show');
-    Route::get('/roles/{role}/edit', [RoleController::class, 'edit'])->name('roles.edit');
-    Route::put('/roles/{role}', [RoleController::class, 'update'])->name('roles.update');
-    Route::delete('/roles/{role}', [RoleController::class, 'destroy'])->name('roles.destroy');
-    Route::get('/roles/get-data', [RoleController::class, 'getData'])->name('roles.getData');
+     Route::resource('roles', RoleController::class)->middleware([
+        'permission:read roles'
+    ]);
 });
 
 
